13f007b656817ec6aa7c99a2509f48548c82d803
a646911949 <646911949@qq.com>
2018-03-29 13:40:02 +0800

    封装了个泛泛，修改90°

--- src/main/java/com/zmedu/pic/Capture.java
+++ src/main/java/com/zmedu/pic/Capture.java
@@ -1,4 +1,5 @@
 package com.zmedu.pic;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -7,15 +8,23 @@ import javax.imageio.ImageIO;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamResolution;
 
-public class Capture
-{
+public class Capture {
 
-    public static void main(String[] args) throws IOException
-    {
-    	Webcam webcam = Webcam.getDefault();
-		webcam.setCustomViewSizes(WebcamResolution.VGA.getSize());
-		webcam.setViewSize(WebcamResolution.VGA.getSize());
-    	webcam.open();
-    	ImageIO.write(webcam.getImage(), "JPG", new File("pic.jpg"));
-    }
+	public static void main(String[] args) throws IOException {
+		new Capture().capture("D:/pic.jpg");
+	}
+
+	private boolean capture(String path) {
+		try {
+			Webcam webcam = Webcam.getDefault();
+			webcam.setCustomViewSizes(WebcamResolution.VGA.getSize());
+			webcam.setViewSize(WebcamResolution.VGA.getSize());
+			webcam.open();
+			ImageIO.write(webcam.getImage(), "JPG", new File(path));
+			return true;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
 }
